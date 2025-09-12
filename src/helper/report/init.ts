26fc9693dd982b67b334d7bec0ae4d92f851e3e6
eyup <eyuptozcu@gmail.com>
2025-09-12 12:36:25 +0100

    fix: remove emptyDir call for allure-results in init script

--- src/helper/report/init.ts
+++ src/helper/report/init.ts
@@ -2,7 +2,6 @@ const fs = require("fs-extra");
 try {
     fs.ensureDir("test-results");
     fs.emptyDir("test-results");
-    fs.emptyDir("allure-results");
     console.log("Folders created!");
 } catch (error) {
     console.log("Folder not created! " + error);
